5ea7e81ce1d1db4970c08a9971c74424217f87a4
PigeonGrass841 <98603652+PigeonGrass841@users.noreply.github.com>
2022-11-29 00:08:41 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // e: Entrée
 // d: Dessert
 // m: Meal
-initialize {
+function initialize() {
     aProteins = 0;
     eProteins = 0;
     dProteins = 0;
@@ -24,6 +24,23 @@ initialize {
     eCalories = 0;
     dCalories = 0;
     mCalories = 0;
+
+    display("aProteins", aProteins);
+    display("aCarbohydrates", aCarbohydrates);
+    display("aFats", aFats);
+    display("aCalories", aCalories);
+
+    display("eProteins", eProteins);
+    display("eCarbohydrates", eCarbohydrates);
+    display("eFats", eFats);
+    display("eCalories", eCalories);
+
+    display("dProteins", dProteins);
+    display("dCarbohydrates", dCarbohydrates);
+    display("dFats", dFats);
+    display("dCalories", dCalories);
+
+    display("mCalories", mCalories);
 }
 
 // Increases the total number of GRAMS for the food by 1
@@ -47,7 +64,7 @@ function calcGrams(appetizer, entrée, dessert) {
 }
 
 // Calculates the total number of CALORIES for the food
-function calcCalories(calories, proteins, carbohydrates, fats) {
+function calcCalories(proteins, carbohydrates, fats) {
     var calories = (proteins * 4) + (carbohydrates * 4) + (fats * 9);
     return calories;
 }
@@ -79,5 +96,197 @@ aProteinsMinus.onclick = function() {
     mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
     display("aProteins", aProteins);
     display("aCalories", aCalories);
+    display("mCalories", mCalories);
+}
+
+// Increases the total number of CARBOHYDRATES for the APPETIZER by 1
+var aCarbohydratesPlus = document.getElementById("aCarbohydratesPlus");
+aCarbohydratesPlus.onclick = function() {
+    aCarbohydrates = increment(aCarbohydrates);
+    mCarbohydrates = calcGrams(aCarbohydrates, eCarbohydrates, dCarbohydrates);
+    aCalories = calcCalories(aProteins, aCarbohydrates, aFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("aCarbohydrates", aCarbohydrates);
+    display("aCalories", aCalories);
+    display("mCalories", mCalories);
+}
+
+// Decreases the total number of CARBOHYDRATES for the APPETIZER by 1
+var aCarbohydratesMinus = document.getElementById("aCarbohydratesMinus");
+aCarbohydratesMinus.onclick = function() {
+    aCarbohydrates = decrement(aCarbohydrates);
+    mCarbohydrates = calcGrams(aCarbohydrates, eCarbohydrates, dCarbohydrates);
+    aCalories = calcCalories(aProteins, aCarbohydrates, aFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("aCarbohydrates", aCarbohydrates);
+    display("aCalories", aCalories);
+    display("mCalories", mCalories);
+}
+
+// Increases the total number of FATS for the APPETIZER by 1
+var aFatsPlus = document.getElementById("aFatsPlus");
+aFatsPlus.onclick = function() {
+    aFats = increment(aFats);
+    mFats = calcGrams(aFats, eFats, dFats);
+    aCalories = calcCalories(aProteins, aCarbohydrates, aFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("aFats", aFats);
+    display("aCalories", aCalories);
+    display("mCalories", mCalories);
+}
+
+// Decreases the total number of FATS for the APPETIZER by 1
+var aFatsMinus = document.getElementById("aFatsMinus");
+aFatsMinus.onclick = function() {
+    aFats = decrement(aFats);
+    mFats = calcGrams(aFats, eFats, dFats);
+    aCalories = calcCalories(aProteins, aCarbohydrates, aFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("aCarbohydrates", aCarbohydrates);
+    display("aCalories", aCalories);
+    display("mCalories", mCalories);
+}
+
+// Increases the total number of PROTEINS for the ENTRÉE by 1
+var eProteinsPlus = document.getElementById("eProteinsPlus");
+eProteinsPlus.onclick = function() {
+    eProteins = increment(eProteins);
+    mProteins = calcGrams(aProteins, eProteins, dProteins);
+    eCalories = calcCalories(eProteins, eCarbohydrates, eFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("eProteins", eProteins);
+    display("eCalories", eCalories);
+    display("mCalories", mCalories);
+}
+
+// Decreases the total number of PROTEINS for the ENTRÉE by 1
+var eProteinsMinus = document.getElementById("eProteinsMinus");
+eProteinsMinus.onclick = function() {
+    eProteins = decrement(eProteins);
+    mProteins = calcGrams(aProteins, eProteins, dProteins);
+    eCalories = calcCalories(eProteins, eCarbohydrates, eFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("eProteins", eProteins);
+    display("eCalories", eCalories);
+    display("mCalories", mCalories);
+}
+
+// Increases the total number of CARBOHYDRATES for the ENTRÉE by 1
+var eCarbohydratesPlus = document.getElementById("eCarbohydratesPlus");
+eCarbohydratesPlus.onclick = function() {
+    eCarbohydrates = increment(eCarbohydrates);
+    mCarbohydrates = calcGrams(aCarbohydrates, eCarbohydrates, dCarbohydrates);
+    eCalories = calcCalories(eProteins, eCarbohydrates, eFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("eCarbohydrates", eCarbohydrates);
+    display("eCalories", eCalories);
+    display("mCalories", mCalories);
+}
+
+// Decreases the total number of CARBOHYDRATES for the ENTRÉE by 1
+var eCarbohydratesMinus = document.getElementById("eCarbohydratesMinus");
+eCarbohydratesMinus.onclick = function() {
+    eCarbohydrates = decrement(eCarbohydrates);
+    mCarbohydrates = calcGrams(aCarbohydrates, eCarbohydrates, dCarbohydrates);
+    eCalories = calcCalories(eProteins, eCarbohydrates, eFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("eCarbohydrates", eCarbohydrates);
+    display("eCalories", eCalories);
+    display("mCalories", mCalories);
+}
+
+// Increases the total number of FATS for the ENTRÉE by 1
+var eFatsPlus = document.getElementById("eFatsPlus");
+eFatsPlus.onclick = function() {
+    eFats = increment(eFats);
+    mFats = calcGrams(aFats, eFats, dFats);
+    eCalories = calcCalories(eProteins, eCarbohydrates, eFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("eFats", eFats);
+    display("eCalories", eCalories);
+    display("mCalories", mCalories);
+}
+
+// Decreases the total number of FATS for the ENTRÉE by 1
+var eFatsMinus = document.getElementById("eFatsMinus");
+eFatsMinus.onclick = function() {
+    eFats = decrement(eFats);
+    mFats = calcGrams(aFats, eFats, dFats);
+    eCalories = calcCalories(eProteins, eCarbohydrates, eFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("eCarbohydrates", eCarbohydrates);
+    display("eCalories", eCalories);
+    display("mCalories", mCalories);
+}
+
+// Increases the total number of PROTEINS for the DESSERTS by 1
+var dProteinsPlus = document.getElementById("dProteinsPlus");
+dProteinsPlus.onclick = function() {
+    dProteins = increment(dProteins);
+    mProteins = calcGrams(aProteins, eProteins, dProteins);
+    dCalories = calcCalories(dProteins, dCarbohydrates, dFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("dProteins", dProteins);
+    display("dCalories", dCalories);
+    display("mCalories", mCalories);
+}
+
+// Decreases the total number of PROTEINS for the DESSERTS by 1
+var dProteinsMinus = document.getElementById("dProteinsMinus");
+dProteinsMinus.onclick = function() {
+    dProteins = decrement(dProteins);
+    mProteins = calcGrams(aProteins, eProteins, dProteins);
+    dCalories = calcCalories(dProteins, dCarbohydrates, dFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("dProteins", dProteins);
+    display("dCalories", dCalories);
+    display("mCalories", mCalories);
+}
+
+// Increases the total number of CARBOHYDRATES for the DESSERTS by 1
+var dCarbohydratesPlus = document.getElementById("dCarbohydratesPlus");
+dCarbohydratesPlus.onclick = function() {
+    dCarbohydrates = increment(dCarbohydrates);
+    mCarbohydrates = calcGrams(aCarbohydrates, eCarbohydrates, dCarbohydrates);
+    dCalories = calcCalories(dProteins, dCarbohydrates, dFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("dCarbohydrates", dCarbohydrates);
+    display("dCalories", dCalories);
+    display("mCalories", mCalories);
+}
+
+// Decreases the total number of CARBOHYDRATES for the DESSERTS by 1
+var dCarbohydratesMinus = document.getElementById("dCarbohydratesMinus");
+dCarbohydratesMinus.onclick = function() {
+    dCarbohydrates = decrement(dCarbohydrates);
+    mCarbohydrates = calcGrams(aCarbohydrates, eCarbohydrates, dCarbohydrates);
+    dCalories = calcCalories(dProteins, dCarbohydrates, dFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("dCarbohydrates", dCarbohydrates);
+    display("dCalories", dCalories);
+    display("mCalories", mCalories);
+}
+
+// Increases the total number of FATS for the DESSERTS by 1
+var dFatsPlus = document.getElementById("dFatsPlus");
+dFatsPlus.onclick = function() {
+    dFats = increment(dFats);
+    mFats = calcGrams(aFats, eFats, dFats);
+    dCalories = calcCalories(dProteins, dCarbohydrates, dFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("dFats", dFats);
+    display("dCalories", dCalories);
+    display("mCalories", mCalories);
+}
+
+// Decreases the total number of FATS for the DESSERTS by 1
+var dFatsMinus = document.getElementById("dFatsMinus");
+dFatsMinus.onclick = function() {
+    dFats = decrement(dFats);
+    mFats = calcGrams(aFats, eFats, dFats);
+    dCalories = calcCalories(dProteins, dCarbohydrates, dFats);
+    mCalories = calcCalories(mProteins, mCarbohydrates, mFats);
+    display("dCarbohydrates", dCarbohydrates);
+    display("dCalories", dCalories);
     display("mCalories", mCalories);
 }
